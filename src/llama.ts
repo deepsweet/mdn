2528abb3f743991de390ef896870a0c04156f4ee
deepsweet <kir@belevi.ch>
2026-04-01 17:34:46 +0300

    🐞 set llama.cpp logging level to `error`

--- src/llama.ts
+++ src/llama.ts
@@ -1,11 +1,11 @@
-import { getLlama } from 'node-llama-cpp'
+import { getLlama, LlamaLogLevel } from 'node-llama-cpp'
 import type { LlamaEmbeddingContext } from 'node-llama-cpp'
 
 const MAX_TOKENS = 8192
 
 // https://node-llama-cpp.withcat.ai/guide/embedding
 export const getLlamaContext = async (modelPath: string): Promise<LlamaEmbeddingContext> => {
-  const llama = await getLlama()
+  const llama = await getLlama({ logLevel: LlamaLogLevel.error })
   const model = await llama.loadModel({ modelPath })
   const context = await model.createEmbeddingContext({
     contextSize: MAX_TOKENS,
