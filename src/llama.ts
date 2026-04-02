30d35ca3b1adacc8ca8052f367d4d83862ffbbf2
deepsweet <kir@belevi.ch>
2026-04-02 18:10:43 +0300

    🛠️ tweak llama.cpp embedding threads

--- src/llama.ts
+++ src/llama.ts
@@ -1,3 +1,4 @@
+import os from 'node:os'
 import { getLlama, LlamaLogLevel } from 'node-llama-cpp'
 import type { LlamaEmbeddingContext } from 'node-llama-cpp'
 
@@ -5,12 +6,13 @@ const MAX_TOKENS = 8192
 
 // https://node-llama-cpp.withcat.ai/guide/embedding
 export const getLlamaContext = async (modelPath: string): Promise<LlamaEmbeddingContext> => {
+  const threads = Math.floor(os.availableParallelism() / 2)
   const llama = await getLlama({ logLevel: LlamaLogLevel.error })
   const model = await llama.loadModel({ modelPath })
   const context = await model.createEmbeddingContext({
     contextSize: MAX_TOKENS,
     batchSize: MAX_TOKENS,
-    threads: 0
+    threads
   })
 
   context.onDispose.createOnceListener(() => {
