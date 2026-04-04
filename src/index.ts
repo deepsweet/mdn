d735d934edadc634b93c0b464514ffa17447c3b3
deepsweet <kir@belevi.ch>
2026-04-04 12:21:58 +0300

    🛠️ extract downloading helper

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
-import { env } from './env.ts'
-import { downloadDataset, downloadModel } from './huggingface.ts'
+import { downloadDatasetAndModel } from './download.ts'
 import { startMcpServer } from './server.ts'
 
 switch (process.argv[2]) {
   case 'download': {
-    if (env.MDN_DATASET_PATH == null) {
-      const locale = process.argv[3] ?? env.MDN_DATASET_LOCALE
-
-      await downloadDataset(locale)
-    }
-
-    if (env.MDN_MODEL_PATH == null) {
-      await downloadModel()
-    }
-
+    await downloadDatasetAndModel(process.argv[3])
     break
   }
   case 'server': {
