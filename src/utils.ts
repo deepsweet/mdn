7be9db3774c55f05a7bd5e64960955b538faa8a2
deepsweet <kir@belevi.ch>
2026-04-04 12:23:57 +0300

    🛠️ refactor using more utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
 export const getTableName = (locale: string): string => {
   return `mdn-${locale}`
 }
+
+export const getTableFileName = (locale: string): string => {
+  const tableName = getTableName(locale)
+
+  return `${tableName}.lance`
+}
+
+export const getCacheFileName = (locale: string): string => {
+  return `cache-${locale}.json`
+}
